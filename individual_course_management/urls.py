55e2d91693eee4a072be9d88ae9e9ad0ba0bc339
sina karamiyan <sinakn78@gmail.com>
2024-11-20 14:41:20 +0330

    " Add view for get lesson and related contents  and render in various template"

--- individual_course_management/urls.py
+++ individual_course_management/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Dashboard, Chapters
+from .views import Dashboard, Chapters, Contents
 
 urlpatterns = [
     path("dashboard/", Dashboard.as_view(), name="dashboard"),
-    path("<int:course_id>/", Chapters.as_view(), name="chapters"),
+    path("chapters/<int:course_id>/", Chapters.as_view(), name="chapters"),
+    path("lessons/content/<int:content_id>", Contents.as_view(), name="Contents"),
 ]
--- individual_course_management/views.py
+++ individual_course_management/views.py
@@ -5,6 +5,7 @@ from django.views.generic import TemplateView
 from .models import Strike, Enrolment, RoleAssignment
 from .models import Course, CourseEnroll, Chapter, ChapterComplete, Lesson, Content, ContentComplete, Template, Quiz, QuizSubmit, ShortQuiz, ShortQuizSubmit
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 class Dashboard(LoginRequiredMixin,TemplateView):
@@ -44,17 +45,18 @@ class Chapters(LoginRequiredMixin, TemplateView):
     def get_context_data(self, **kwargs):        
         # get course based on selected course in dashboard
         course_id = self.kwargs.get('course_id')
-        enrolment = Enrolment.objects.filter(role_assignment__user=self.request.user, course_enroll__course=course_id).select_related('role_assignment','course_enroll', 'course_enroll__course', 'course_enroll__course__course_group').first()
+        enrolment = Enrolment.objects.filter(role_assignment__user=self.request.user, course_enroll__course=course_id).select_related('role_assignment','course_enroll__course__course_group').first()
         if not enrolment:
             return { 'course':''}
         
-        # steps parital page
-        chapters = Chapter.objects.filter(course=enrolment.course_enroll.course).order_by('index').select_related('course')
+        chapters = Chapter.objects.filter(course=enrolment.course_enroll.course).select_related('course').order_by('index')
+        
+        # find first content related to each lesson and chapter        
+        first_contents = list(Content.objects.filter(lesson__chapter__in=chapters).select_related('lesson').distinct('lesson__chapter'))
         first_chapter = chapters.first()
-        last_chapter = chapters.last()
-
+        
         # find last completed chapter
-        last_completed_chapter = ChapterComplete.objects.filter(enrolment=enrolment).last()
+        last_completed_chapter = ChapterComplete.objects.filter(enrolment=enrolment).select_related('enrolment').last()
         
         # Find the next chapter after the last completed chapter
         if last_completed_chapter: 
@@ -64,12 +66,29 @@ class Chapters(LoginRequiredMixin, TemplateView):
 
         context = {
             'course': enrolment.course_enroll.course,
-            'course_group_title': enrolment.course_enroll.course.course_group.title,
-            'chapters': chapters,
-            'first_chapter': first_chapter,
-            'last_chapter': last_chapter,
             'last_completed_chapter': last_completed_chapter,
             'active_chapter': next_chapter,
+            'first_contents': first_contents
         }
         return context
     
+class Contents(LoginRequiredMixin, TemplateView):
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        return render(request, "individual_course_management/lesson/lesson.html", context)
+    
+    def get_context_data(self, **kwargs):
+        content_id = self.kwargs.get('content_id')
+        content = Content.objects.filter(id=content_id).first()
+
+        # List of related Contents for handle paginator
+        contents = Content.objects.filter(lesson=content.lesson).select_related('lesson')
+        paginator = Paginator(contents, per_page=1)
+        page_obj = paginator.get_page(content.index)
+        
+        context = {
+            'page_obj': page_obj,
+            # 'content': page_obj.object_list[0],
+            'content': content,
+        }
+        return context
